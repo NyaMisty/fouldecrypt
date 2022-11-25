8aaf8b51e017ebca151f2b7a613eab650d72d6c4
und3fined <me@und3fined.com>
2022-11-25 14:30:12 +0700

    disable VERBOSE

--- foulwrapper.m
+++ foulwrapper.m
@@ -8,7 +8,7 @@
 #import <MobileContainerManager/MCMContainer.h>
 #import <MobileCoreServices/LSApplicationProxy.h>
 
-static int VERBOSE = 0;
+// static int VERBOSE = 0;
 
 #define MH_MAGIC_64   0xfeedfacf  /* the 64-bit mach magic number */
 #define MH_CIGAM_64   0xcffaedfe  /* NXSwapInt(MH_MAGIC_64) */
