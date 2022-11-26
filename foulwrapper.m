259a8b1d588f5dc1a2f990f2f2e2576ec774d077
und3fined <me@und3fined.com>
2022-11-26 10:32:07 +0700

    save

--- foulwrapper.m
+++ foulwrapper.m
@@ -203,7 +203,7 @@ main(int argc, char *argv[])
         return didError;
     }
     
-    fprintf(stderr, "[dump] Total: %d\n", [decryptCount integerValue]);
+    fprintf(stderr, "[dump] Total: %s\n", [NSString stringWithFormat:@"%i", decryptCount.intValue]);
 
     if ([decryptCount integerValue] == 0) {
         fprintf(stderr, "[dump] no Mach-O found\n");
